6a75cf180a3cc079473ae900b306969815edd5ef
Volodymyr Gladun <vgladun@gmail.com>
2019-07-16 17:46:05 +0100

    Mocks refactoring RentalController implemented

--- test/TestCustomer.cpp
+++ test/TestCustomer.cpp
@@ -1,23 +1,8 @@
-#include "catch.hpp"
-#include "catch2/trompeloeil.hpp"
-
 #include "Customer.h"
+#include "mocks/MockRentable.h"
 
 using trompeloeil::deathwatched;
 
-
-
-class MockRentable : public IRentable {
-    MAKE_CONST_MOCK0(Title, const std::string&(), override);
-    MAKE_CONST_MOCK0(Type, const std::string&(), override);
-    MAKE_CONST_MOCK0(RentalStart, clock_time(), override);
-    MAKE_CONST_MOCK0(RentalEnd, clock_time(), override);
-    MAKE_CONST_MOCK0(CostPerDay, Money(), override);
-    MAKE_CONST_MOCK0(PenaltyPerDay, Money(), override);
-
-    MAKE_MOCK1(RentalStart, void(clock_time), override);
-    MAKE_MOCK1(RentalEnd, void(clock_time), override);
-};
 
 TEST_CASE("Customer class correctly assign name John Smith") {
     Customer custImpl{"John Smith", ""};
---
+++ test/TestRentalController.cpp
@@ -0,0 +1,48 @@
+#include "interface/ICustomer.h"
+#include "interface/IRentable.h"
+
+class RentalController {
+public:
+    void IssueRental(ICustomer& customer,
+                     std::unique_ptr<IRentable> rentable,
+                     IRentable::clock_time endTime) {
+        rentable->RentalStart(std::chrono::system_clock::now());
+        rentable->RentalEnd(endTime);
+        customer.AssignRental(std::move(rentable));
+    }
+};
+
+#include "mocks/MockRentable.h"
+#include "mocks/MockCustomer.h"
+
+using trompeloeil::_;
+using trompeloeil::eq;
+using namespace std::chrono_literals;
+using std::chrono::system_clock;
+
+TEST_CASE("An IRentable is correctly dated and given to a ICustomer") {
+    //Given a rental controller
+    RentalController rc{};
+
+    //When we create some mocks
+    auto mockRentable = std::make_unique<MockRentable>();
+    MockCustomer mockCustomer;
+
+    //And expect the customer to be given a rental
+    auto ptrVal = mockRentable.get();
+    REQUIRE_CALL(mockCustomer, AssignRental(_))
+        .WITH(_1.get() == ptrVal);
+
+    //And expect dates to be set
+    auto expiry = system_clock::now() + 100min;
+    REQUIRE_CALL(*mockRentable, RentalStart(_))
+        .WITH(_1 <= system_clock::now());
+    REQUIRE_CALL(*mockRentable, RentalEnd(_))
+        .WITH(_1 == expiry);
+
+    rc.IssueRental(mockCustomer, std::move(mockRentable), expiry);
+}
+
+TEST_CASE("A RentalController correctly takes back a rental") {
+
+}
